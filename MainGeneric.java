5842c9026c47a6369471fdb7482f5b64a4462275
rizqiaditya222 <160991358+rizqiaditya222@users.noreply.github.com>
2024-05-04 16:49:20 +0700

    Delete MainGeneric.java

--- MainGeneric.java
+++
@@ -1,9 +0,0 @@
-public class MainGeneric {
-    public static void main(String[] args) {
-        Generic<String> objGeneric = new Generic<String>("halo");
-
-        String input = objGeneric.getDataT();
-        System.out.println(input);
-        objGeneric.displayType();
-    }
-}
